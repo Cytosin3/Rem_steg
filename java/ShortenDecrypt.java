efd608f4f779d304a28998c6f1c6e2ac47961643
Cytosine <34599434+SKE48Cyto@users.noreply.github.com>
2017-12-21 17:01:05 +0800

    Update ShortenDecrypt.java

--- java/ShortenDecrypt.java
+++ java/ShortenDecrypt.java
@@ -9,7 +9,7 @@ public class ShortenDecrypt {
 		BufferedImage secretImg = ImageIO.read(new File("Rem_secret.png"));
 		for (int w = 0; w < secretImg.getWidth(); w++)
 			for (int h = 0; h < secretImg.getHeight(); h++) {
-				int lsb = new Color(secretImg.getRGB(w, h)).getRed() % 2; // 个人建议把 % 2 写成 >> 1
+				int lsb = new Color(secretImg.getRGB(w, h)).getRed() % 2; // 个人建议把 % 2 写成 & 1
 				secretImg.setRGB(w, h, (lsb == 0 ? new Color(0, 0, 0) : new Color(255, 255, 255)).getRGB());
 			}
 		ImageIO.write(secretImg, "png", new File("secret.png"));
